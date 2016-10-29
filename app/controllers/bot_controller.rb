1058f0b5b75bcd92a056b3937fd758ae55c21c4d
Mitch Cloney <moemyintkoko99@gmail.com>
2016-10-29 09:24:52 -0700

    shit

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -20,28 +20,49 @@ class BotController < ApplicationController
       end
       unless text == "aaa"
         if greeting.include? text
-          mes = {
-            "attachment":{
-              "type":"template",
-              "payload":{
-                "template_type":"button",
-                "text": "",
-                "buttons":[
-                  {
-                    "type":"postback",
-                    "title":"FAQs",
-                    "payload":"faq"
-                  }
-                ]
-              }
-            }
-          }
           FacebookBot.new.send_generic_message(sender, choose_topic)
-          FacebookBot.new.send_generic_message(sender, mes)
         elsif text == "atmservice" || text == "ATM Services"
           FacebookBot.new.send_generic_message(sender, choose_atm_topic)
+        elsif text == "more" || text == "More ..."
+          FacebookBot.new.send_generic_message(sender, choose_more)
         elsif text == "faq" || text == "FAQs"
-          FacebookBot.new.send_text_message(sender, "hi")
+          FacebookBot.new.send_generic_message(sender, choose_faq)
+        elsif text == "lostcard" || text == "I lost my credit card"
+          res = "Please call the Bank’s 24 hour customer service hotline immediately to report:
+- For Platinum Card +95 137 0066
+- For Classic Card  +95 137 0055"
+          FacebookBot.new.send_text_message(sender, res)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "whatiscard" || text == "What is Credit Card?"
+          res = "Credit Card is a kind of unsecured personal loan provided through a “plastic card”, which can be used for payment of goods & services or cash withdrawal."
+          FacebookBot.new.send_text_message(sender, res)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "atecard" || text == "ATM ate my card."
+          res = "Please call the Bank’s 24 hour customer service hotline immediately to report:
+- (+95) 1-515216-18"
+          FacebookBot.new.send_text_message(sender, res)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "tempblock" || text == "Want to do temporary block."
+          res = "Please call the Bank’s 24 hour customer service hotline immediately to report:
+- (+95) 1-515216-18"
+          FacebookBot.new.send_text_message(sender, res)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "newchip" || text == "Want new chip card?"
+          res = "At a chip-enabled terminal
+- Instead of swiping, insert your card face up into the terminal
+- Don’t take the card out until the transaction is complete
+- Sign for your purchase or enter your PIN, if asked, and remove the card."
+          res1 = "At a traditional terminal without chip technology
+- Just swipe your card as you’ve done in the past
+- Sign for your purchase."
+          res2 = "If asked For phone or online purchases
+- Complete your purchases just as you’ve done in the past."
+          FacebookBot.new.send_text_message(sender, res)
+          FacebookBot.new.send_text_message(sender, res1)
+          FacebookBot.new.send_text_message(sender, res2)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "morefaq" || text == "More .."
+          FacebookBot.new.send_generic_message(sender, choose_faq_more)
         elsif text == "ျပန္လည္ေရြးခ်ယ္မည္"
           FacebookBot.new.send_generic_message(sender, choose_again_support)
         elsif text == "ျပန္လည္ေရြးခ်ယ္မည္။"
@@ -49,14 +70,217 @@ class BotController < ApplicationController
         elsif text == "မေရြးခ်ယ္ေတာ့ပါ။"
           FacebookBot.new.send_generic_message(sender, generic)
         elsif text == "အစသုိ႔ျပန္သြားမည္။"
-          choose_topic[:attachment][:payload].merge!(text: "သိရွိလုိသည့္ အေၾကာင္းအရာကုိ ျပန္လည္ေရြးခ်ယ္ပါ။")
+          choose_topic[:attachment][:payload].merge!(text: "သိရွိလုိသည့္ အေၾကာင္းအရာကုိ ျပန္လည္ေရြးခ်ယ္ပါ။")
           FacebookBot.new.send_generic_message(sender, choose_topic)
         elsif text == "မလုပ္ေဆာင္ေတာ့ပါ။"
           FacebookBot.new.send_generic_message(sender, generic)
+        elsif text == "Bahan" || text == "bahan" || text == "ba han" || text == "Ba Han"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Bahan",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/KBZ+ATM,+U+Chit+Maung+Rd,+Yangon/@16.816299,96.0980961,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c1ecb385a272a3:0x133a3a299e9803da!2m2!1d96.1681366!2d16.8163109",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Kamaryut" || text == "Kamayut" || text == "ka ma yut" || text == "ka mar yut" ||
+          text == "kamaryut" || text == "kamayut"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Kamaryut",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/Kanbawza+Bank,+Kamaryut+Branch,+Yangon/@16.8325306,96.0623348,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c194b7f7af0dbd:0x920350f3e1dc3a1c!2m2!1d96.1323753!2d16.8325425",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Sanchaung" || text == "sanchaung" || text == "san chaung" || text == "San Chaung"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Sanchaung",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/KBZ+ATM,+Hanthawaddy+Road,+Yangon/@16.8136185,96.0630624,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c1eb4995f06a93:0x78f819b245bd935b!2m2!1d96.1331029!2d16.8136304",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Ahlone" || text == "ahlone" || text == "a lone" || text == "A lone" || text == "Ah Lone"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Ahlone",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/search/kbz+bank+in+Ahlone,+Yangon,+Yangon+Region/@16.771094,96.1512332,15z/data=!3m1!4b1",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Dagon" || text == "dagon" || text == "da gon"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Dagon",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/KBZ+ATM,+Hanthawaddy+Road,+Yangon/@16.8136185,96.0630624,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c1eb4995f06a93:0x78f819b245bd935b!2m2!1d96.1331029!2d16.8136304",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Tamwe" || text == "tarmwe" || text == "Tar Mwe" || text == "Tar Mway" || text == "tar mwe"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Tamwe",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/KBZ+ATM,+U+Chit+Maung+Rd,+Yangon/@16.816299,96.0980961,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c1ecb385a272a3:0x133a3a299e9803da!2m2!1d96.1681366!2d16.8163109",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Pabedan" || text == "pabedan" || text == "Pa Be Dan" || text == "pa be dan"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Pabedan",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/KBZ+ATM,+U+Chit+Maung+Rd,+Yangon/@16.816299,96.0980961,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c1ecb385a272a3:0x133a3a299e9803da!2m2!1d96.1681366!2d16.8163109",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Latha" || text == "Lathar" || text == "La Tha" || text == "La Thar" || text == "la thar" ||
+          text == "la tha"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Latha",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/KBZ+ATM,+U+Chit+Maung+Rd,+Yangon/@16.816299,96.0980961,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c1ecb385a272a3:0x133a3a299e9803da!2m2!1d96.1681366!2d16.8163109",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
+        elsif text == "Hlaing" || text == "hlaing"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Hlaing",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/Kanbawza+Bank,+Kamaryut+Branch,+Yangon/@16.8325306,96.0623348,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c194b7f7af0dbd:0x920350f3e1dc3a1c!2m2!1d96.1323753!2d16.8325425",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support) 
+        elsif text == "Lanmadaw" || text == "lanmadaw" || text == "lan ma daw" || text == "Lan Ma Daw"
+          mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"ATM in Lanmadaw",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://www.google.com.mm/maps/dir/''/Kanbawza+Bank,+Kamaryut+Branch,+Yangon/@16.8325306,96.0623348,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x30c194b7f7af0dbd:0x920350f3e1dc3a1c!2m2!1d96.1323753!2d16.8325425",
+                    "title":"View On Map",
+                    "webview_height_ratio": "compact"
+                  }
+                ]
+              }
+            }
+          }
+          FacebookBot.new.send_generic_message(sender, mes)
+          FacebookBot.new.send_generic_message(sender, back_support)
         elsif text == "call"
           FacebookBot.new.send_text_message(sender, "Please Dial '01-538075', '01-538076', '01-538078'")
+          FacebookBot.new.send_generic_message(sender, back_support)
         elsif text == "atmlocation" || text == "ATM Locations"
-          res = "ေအာက္ေဖာ္ျပပါ ၿမိဳ႔နယ္မ်ားအနက္မွ ရွာေဖြလုိေသာ ၿမိဳ႕နယ္၏အမည္အား ေဖာ္ျပပါအတုိင္း ရုိက္ထည့္ပါ။"
+          res = "ေအာက္ေဖာ္ျပပါ ၿမိဳ႔နယ္မ်ားအနက္မွ ရွာေဖြလုိေသာ ၿမိဳ႕နယ္၏အမည္အား ေဖာ္ျပပါအတုိင္း ရုိက္ထည့္ပါ။"
           mes = "- Ahlone
 - Bahan
 - Dagon
@@ -137,7 +361,7 @@ class BotController < ApplicationController
               "type":"template",
               "payload":{
                 "template_type":"button",
-                "text":"မဂၤလာပါရွင္။ ယခုလုိဆက္သြယ္ျခင္းအတြက္ ေက်းဇူးတင္ရွိပါတယ္။ မည္သည့္အေၾကာင္းအရာအတြက္ သိရွိလုိပါသလဲ။",
+                "text":"မဂၤလာပါရွင္။ ယခုလုိဆက္သြယ္ျခင္းအတြက္ ေက်းဇူးတင္ရွိပါတယ္။ မည္သည့္အေၾကာင္းအရာအတြက္ သိရွိလုိပါသလဲ။",
                 "buttons":[
                   {
                     "type":"postback",
@@ -151,8 +375,95 @@ class BotController < ApplicationController
                   },
                   {
                     "type":"postback",
+                    "title":"More ...",
+                    "payload":"more"
+                  }
+                ]
+              }
+            }
+          }
+    end
+
+    def choose_faq
+      mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"Frequently Asked Questions.",
+                "buttons":[
+                  {
+                    "type":"postback",
+                    "title":"I lost my credit card",
+                    "payload":"lostcard"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"What is Credit Card?",
+                    "payload":"whatiscard"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"More ..",
+                    "payload":"morefaq"
+                  }
+                ]
+              }
+            }
+          }
+    end
+
+    def choose_faq_more
+      mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"Frequently Asked Questions.",
+                "buttons":[
+                  {
+                    "type":"postback",
+                    "title":"ATM ate my card.",
+                    "payload":"atecard"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Want to do temporary block.",
+                    "payload":"tempblock"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Want new chip card?",
+                    "payload":"newchip"
+                  }
+                ]
+              }
+            }
+          }
+    end
+
+    def choose_more
+      mes = {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"button",
+                "text":"သိရွိလုိေသာအေၾကာင္းအရာကို ထပ္မံေရြးခ်ယ္ပါ။",
+                "buttons":[
+                  {
+                    "type":"postback",
                     "title":"Career",
                     "payload":"career"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"FAQs",
+                    "payload":"faq"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Contact",
+                    "payload":"call"
                   }
                 ]
               }
@@ -238,7 +549,7 @@ class BotController < ApplicationController
               "type":"template",
               "payload":{
                 "template_type":"button",
-                "text":"သိရွိလုိသည့္ အမ်ဴိးအစားကုိ ျပန္လည္ေရြးခ်ယ္ပါ။",
+                "text":"သိရွိလုိသည့္ အမ်ဴိးအစားကုိ ျပန္လည္ေရြးခ်ယ္ပါ။",
                 "buttons":[
                   {
                     "type":"postback",
@@ -252,8 +563,8 @@ class BotController < ApplicationController
                   },
                   {
                     "type":"postback",
-                    "title":"Career",
-                    "payload":"career"
+                    "title":"More ...",
+                    "payload":"more"
                   }
                 ]
               }
